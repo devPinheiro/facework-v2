d9b47f24261d49b25e6ef3984146a9131519971b
devPinheiro <samuelpinheiro41@gmail.com>
2020-07-17 20:59:16 +0100

    WIP feeds

---
+++ client/components/AuthorCard/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const AuthorCard = props => {
+    return (
+        <div className="md:flex bg-white rounded-lg p-6 shadow-lg">
+            <img
+                className="h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6"
+                src="../../assets/image/bedroom.jpg"
+            />
+            <div className="text-center md:text-left">
+                <h2 className="text-lg text-black">Erin Lindford</h2>
+                <div className="text-purple-500 text-grey-darkest">Product Engineer</div>
+                <div className="text-grey-light">erinlindford@example.com</div>
+                <div className="text-grey-light">(555) 765-4321</div>
+            </div>
+        </div>
+    )
+}
+
+AuthorCard.propTypes = {}
+
+export default AuthorCard
