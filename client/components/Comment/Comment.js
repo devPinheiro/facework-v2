95a9dcc74f740457fd4bdf109edb8b0eed9c821d
devPinheiro <pinheirolaoluwa@gmail.com>
2020-11-09 13:56:07 +0100

    post integration

--- client/components/Comment/Comment.js
+++ client/components/Comment/Comment.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Moment from '../Moment';
 
 
-const Comment = ({ avatar, name, comment, time }) => {
+const Comment = ({ avatar, name, body, created_at }) => {
     return (
         <div>
             <div className="block border-grey-lightest px-1 pt-2">
@@ -11,12 +12,12 @@ const Comment = ({ avatar, name, comment, time }) => {
                             <img class="rounded-full h-6 w-6 object-cover" src={avatar} alt="" />
                               <div className="flex-column ">
                                 <p className="ml-3 text-xs font-thin  text-grey-dark">{name}</p>               
-                                <small className="ml-3 -mt-2 text-xxs font-thin text-grey-light">{time}</small>               
+                                <small className="ml-3 -mt-2 text-xxs font-thin text-grey-light">{<Moment time={created_at} />}</small>               
                               </div>
                             </Link>
     
                         </div>
-                        <div className=" py-2"><p className="text-xxs font-hairline">{comment}</p></div>  
+                        <div className=" py-2"><p className="text-xxs font-hairline">{body}</p></div>  
             </div>
         </div>
     )
