3fa376fb726f669c2c28b3a4112ae594f4ac5452
devPinheiro <pinheirolaoluwa@gmail.com>
2021-11-05 01:08:53 +0100

    UI fix on landng page

--- client/components/Nav/NavBar.js
+++ client/components/Nav/NavBar.js
@@ -1,13 +1,11 @@
-import React, { useState } from 'react';
-import { Link, NavLink } from 'react-router-dom';
-import classJoin from 'classnames';
-import { FiBell } from "react-icons/fi";
-import styled from 'styled-components';
-import { RiDiscussLine } from "react-icons/ri";
-import NotificationTray from '../NotificationTray';
-import onClickOutside from "react-onclickoutside";
-
-
+import React, { useState } from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import classJoin from 'classnames'
+import { FiBell } from 'react-icons/fi'
+import styled from 'styled-components'
+import { RiDiscussLine } from 'react-icons/ri'
+import NotificationTray from '../NotificationTray'
+import onClickOutside from 'react-onclickoutside'
 
 const UL = styled.ul`
     list-style: none;
@@ -15,107 +13,108 @@ const UL = styled.ul`
     flex-flow: row nowrap;
 
     .nav {
-       
-        
         a {
             text-decoration: none;
             color: #000000;
             font-size: 16px;
-
         }
         .notify-badge {
             left: 11px;
             top: -28px;
             position: relative;
-            border-radius: 100%
+            border-radius: 100%;
         }
     }
-`;
+`
 
 const Navbar = () => {
-  const [isVisible, changeVisibility] = useState(false);
-  const classToggle = classJoin({
-    block: isVisible,
-    hidden: !isVisible,
-  });
-  const [openNotification, setOpenNotification] = useState(false)
-  const [showNotificationBadge, setNotificationBadge] = useState(true)
-  Navbar.handleClickOutside = () => setOpenNotification(false)
+    const [isVisible, changeVisibility] = useState(false)
+    const classToggle = classJoin({
+        block: isVisible,
+        hidden: !isVisible
+    })
+    const [openNotification, setOpenNotification] = useState(false)
+    const [showNotificationBadge, setNotificationBadge] = useState(true)
+    Navbar.handleClickOutside = () => setOpenNotification(false)
 
-  return (
-    <nav className="flex items-center justify-between  bg-white-500 pt-5 z-40 pb-8">
+    return (
+        <nav className="flex items-center justify-between  bg-white-500 pt-5 z-40 pb-8 px-6">
+            <div className="flex items-center flex-shrink-0 text-dark mr-6">
+                <Link to="/" className="text-3xl text-black no-underline">
+                    facework
+                </Link>
+            </div>
+            <div className="block lg:hidden">
+                <button
+                    className="flex items-center px-3 py-2 border navToggler rounded text-dark-200 border-dark-500 hover:text-dark hover:border-dark"
+                    onClick={() => changeVisibility(!isVisible)}
+                >
+                    <svg
+                        className="fill-current h-3 w-3"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <title>Menu</title>
+                        <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+                    </svg>
+                </button>
+            </div>
+            <div
+                id="navLink"
+                className={`${classToggle} w-full block lg:visible xl:visible lg:flex lg:items-center lg:w-auto`}
+            >
+                <div className="lg:flex text-sm text-center m-auto pt-8 lg:pt-1 xl:pt-1">
+                    <NavLink
+                        className="font-raleway block md:text-base lg:text-base xl:text-base text-sm lg:mr-8 mt-4 lg:inline-block lg:mt-0 text-black no-underline text-dark-200 hover:text-dark"
+                        to="/feeds"
+                        exact
+                        activeClassName="border-orange-500 font-semibold pb-1 border-b-2"
+                    >
+                        Home
+                    </NavLink>
+                    <NavLink
+                        className="font-raleway block md:text-base lg:text-base xl:text-base lg:mr-8 mt-4 lg:inline-block lg:mt-0 text-black no-underline hover:text-dark"
+                        to="/jobs"
+                        activeClassName="border-orange-500 font-semibold pb-1 border-b-2"
+                    >
+                        Jobs
+                    </NavLink>
 
-      <div className="flex items-center flex-shrink-0 text-dark mr-6">
-        <Link to="/" className="text-3xl text-black no-underline">
-          facework
-        </Link>
-      </div>
-      <div className="block lg:hidden">
-        <button
-          className="flex items-center px-3 py-2 border navToggler rounded text-dark-200 border-dark-500 hover:text-dark hover:border-dark"
-          onClick={() => changeVisibility(!isVisible)}
-        >
-          <svg
-            className="fill-current h-3 w-3"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <title>Menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-          </svg>
-        </button>
-      </div>
-      <div
-        id="navLink"
-        className={`${classToggle} w-full block lg:visible xl:visible lg:flex lg:items-center lg:w-auto`}
-      >
-        <div className="lg:flex text-sm text-center m-auto pt-8 lg:pt-1 xl:pt-1">
-       
-          <NavLink
-            className="font-raleway block md:text-base lg:text-base xl:text-base text-sm lg:mr-8 mt-4 lg:inline-block lg:mt-0 text-black no-underline text-dark-200 hover:text-dark"
-            to="/feeds"
-            exact
-            activeClassName="border-orange-500 font-semibold pb-1 border-b-2"
-          >
-            Home
-          </NavLink>
-          <NavLink
-            className="font-raleway block md:text-base lg:text-base xl:text-base lg:mr-8 mt-4 lg:inline-block lg:mt-0 text-black no-underline hover:text-dark"
-            to="/jobs"
-            activeClassName="border-orange-500 font-semibold pb-1 border-b-2"
-          >
-            Jobs
-          </NavLink>
+                    <NavLink
+                        className="font-raleway block   md:text-base lg:text-base xl:text-base lg:mr-8 mt-4 lg:inline-block lg:mt-0 text-black no-underline text-dark-200 hover:text-dark"
+                        to="/messages"
+                        exact
+                        activeClassName="border-orange-500 font-semibold pb-1 border-b-2"
+                    >
+                        Chats
+                    </NavLink>
+                    <UL>
+                        <NavLink
+                            to="#"
+                            className="nav px-2 cursor-pointer"
+                            onClick={() =>
+                                setOpenNotification(!openNotification)
+                            }
+                        >
+                            <FiBell />
+                            {showNotificationBadge ? (
+                                <div className="bg-red rounded-full notify-badge h-2 w-2"></div>
+                            ) : null}
+                        </NavLink>
+                    </UL>
 
-          <NavLink
-            className="font-raleway block   md:text-base lg:text-base xl:text-base lg:mr-8 mt-4 lg:inline-block lg:mt-0 text-black no-underline text-dark-200 hover:text-dark"
-            to="/messages"
-            exact
-            activeClassName="border-orange-500 font-semibold pb-1 border-b-2"
-          >
-           Chats
-          </NavLink>
-          <UL>
-            <NavLink to="#" className="nav px-2 cursor-pointer" onClick={() => setOpenNotification(!openNotification)}>
-              
-                    <FiBell />
-                    {showNotificationBadge ? <div className="bg-red rounded-full notify-badge h-2 w-2"></div> : null }
-                  
-            </NavLink>
-          </UL>
-          
-          <NotificationTray openNotification={openNotification} setOpenNotification={setOpenNotification} />
-        </div>
-      </div>
-    </nav>
-  );
-};
+                    <NotificationTray
+                        openNotification={openNotification}
+                        setOpenNotification={setOpenNotification}
+                    />
+                </div>
+            </div>
+        </nav>
+    )
+}
 
 const clickOutsideConfig = {
-  handleClickOutside: () => Navbar.handleClickOutside
-};
-
+    handleClickOutside: () => Navbar.handleClickOutside
+}
 
 export default onClickOutside(Navbar, clickOutsideConfig)
-
-
--- client/components/Nav/SearchForm.js
+++ client/components/Nav/SearchForm.js
@@ -7,43 +7,39 @@ import Textbox from '@components/Textbox'
 const SearchForm = ({ initialValues, onSubmit, validationSchema }) => (
     <Fragment>
         <div className="flex">
-                        <Formik
-                            onSubmit={onSubmit}
-                            initialValues={initialValues}
-                            validationSchema={validationSchema}
-                        >
-                            {({
-                                values,
-                                errors,
-                                touched,
-                                handleChange,
-                                isSubmitting,
-                                handleBlur,
-                                handleSubmit
-                            }) => (
-                                <form onSubmit={handleSubmit}>
-                                    <div className="relative text-gray-600">
-                                    <Textbox
-                                        className="bg-white px-5 pr-10 rounded-full text-sm focus:outline-none"
-                                        type="search"
-                                        name="search"
-                                        label=""
-                                        onBlur={handleBlur}
-                                        value={values.search}
-                                        onChange={handleChange}
-                                        data-testid={'search-field'}
-                                        error={touched.email && errors.email}
-                                        placeholder="Search"
-            
-                                    />
-                                    </div>
-                                        
-                                    
-                                </form>
-                            )}
-                        </Formik>
-
-    </div>
+            <Formik
+                onSubmit={onSubmit}
+                initialValues={initialValues}
+                validationSchema={validationSchema}
+            >
+                {({
+                    values,
+                    errors,
+                    touched,
+                    handleChange,
+                    isSubmitting,
+                    handleBlur,
+                    handleSubmit
+                }) => (
+                    <form onSubmit={handleSubmit}>
+                        <div className="relative text-gray-600">
+                            <Textbox
+                                className="bg-white px-5 pr-10 rounded-full text-sm focus:outline-none"
+                                type="search"
+                                name="search"
+                                label=""
+                                onBlur={handleBlur}
+                                value={values.search}
+                                onChange={handleChange}
+                                data-testid={'search-field'}
+                                error={touched.email && errors.email}
+                                placeholder="Search"
+                            />
+                        </div>
+                    </form>
+                )}
+            </Formik>
+        </div>
     </Fragment>
 )
 
